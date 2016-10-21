df2695248e97bb691cb205a82348b1d245fd3c07
Avi <Avihaihai737@gmail.com>
2016-10-20 18:41:24 -0700

    Create app.js

---
+++ app/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myContacts', [
+  'ngRoute',
+  'firebase',
+  'myContacts.contacts'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/contacts'});
+}]);
